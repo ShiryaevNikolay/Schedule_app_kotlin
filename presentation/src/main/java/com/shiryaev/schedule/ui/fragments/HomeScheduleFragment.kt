d8ba2213e124a55058e7f46d37f84fe84bac140e
ShiryaevNikolay <deendeeps@list.ru>
2021-01-31 20:58:37 +0300

    На главном экране сделал TopBar как custom view и поменял значок сертификации

--- presentation/src/main/java/com/shiryaev/schedule/ui/fragments/HomeScheduleFragment.kt
+++ presentation/src/main/java/com/shiryaev/schedule/ui/fragments/HomeScheduleFragment.kt
@@ -5,20 +5,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.shiryaev.schedule.R
 import com.shiryaev.schedule.databinding.FrHomeScheduleBinding
 import com.shiryaev.schedule.tools.adapters.ViewPagerAdapter
-import com.shiryaev.schedule.ui.views.CustomTabLayout
 import com.shiryaev.schedule.ui.views.utils.*
 
-class HomeScheduleFragment : Fragment(), View.OnClickListener {
+class HomeScheduleFragment : Fragment() {
 
     private var countPage = 0
-    private var showCalendar = false
-    private var showTabs = false
     private var _binding: FrHomeScheduleBinding? = null
     private val binding get() = _binding!!
 
@@ -40,14 +36,10 @@ class HomeScheduleFragment : Fragment(), View.OnClickListener {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding  = FrHomeScheduleBinding.inflate(inflater, container, false)
 
-        initTabLayout(binding.customTabLayout)
         initViewPager(binding)
 
-
-        binding.apply {
-            topBarShowTabsBtn.setOnClickListener(this@HomeScheduleFragment)
-            topBarTitle.setOnClickListener(this@HomeScheduleFragment)
-            changeViewBtn.setOnClickListener(this@HomeScheduleFragment)
+        binding.topBar.onChangeCurrentItem = { selectedTab ->
+            binding.homeScreenVp.currentItem = selectedTab
         }
 
         return binding.root
@@ -58,44 +50,15 @@ class HomeScheduleFragment : Fragment(), View.OnClickListener {
         _binding = null
     }
 
-    override fun onClick(v: View) {
-        when(v.id) {
-            R.id.top_bar_show_tabs_btn, R.id.top_bar_title -> {
-                context?.let { showTabs = showTabs(it, binding.topBarShowTabsBtn, !showTabs) }
-                binding.customTabLayout.isVisible = showTabs
-            }
-            R.id.change_view_btn -> {
-                context?.let { showCalendar = !showCalendar }
-            }
-        }
-    }
-
-    private fun initTabLayout(tabLayout: CustomTabLayout) {
-        tabLayout.run {
-            setCountTab(countPage)
-            setSelectedPage = { selectedTab ->
-                binding.homeScreenVp.currentItem = selectedTab
-            }
-            getSelectedTab = { selectedTab ->
-                setTextTitle(selectedTab)
-            }
-        }
-    }
-
     private fun initViewPager(_binding: FrHomeScheduleBinding) {
-        _binding.homeScreenVp.apply {
+        with (_binding.homeScreenVp) {
             adapter = vpAdapter
             registerOnPageChangeCallback(object : OnPageChangeCallback() {
                 override fun onPageSelected(position: Int) {
                     super.onPageSelected(position)
-                    binding.customTabLayout.setSelectedTab(position)
+                    binding.topBar.setSelectedTab(position)
                 }
             })
         }
-    }
-
-    private fun setTextTitle(position: Int) {
-        val arrayTextTitle = context?.resources?.getStringArray(R.array.full_days_of_week)
-        binding.topBarTitle.text = arrayTextTitle?.get(position)
     }
 }
--- presentation/src/main/java/com/shiryaev/schedule/ui/views/TopBar.kt
+++ presentation/src/main/java/com/shiryaev/schedule/ui/views/TopBar.kt
@@ -3,19 +3,86 @@ package com.shiryaev.schedule.ui.views
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
-import android.widget.LinearLayout
-import android.widget.TableLayout
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.viewpager.widget.ViewPager
-import com.google.android.material.tabs.TabLayout
+import android.widget.FrameLayout
+import androidx.appcompat.widget.AppCompatImageButton
+import androidx.core.view.isVisible
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.textview.MaterialTextView
 import com.shiryaev.schedule.R
+import com.shiryaev.schedule.ui.views.utils.showTabs
 
-class TopBar(
+class TopBar @JvmOverloads constructor(
         context: Context,
         attrs: AttributeSet? = null,
         defStyleAttr: Int = 0
-) : TabLayout(context, attrs, defStyleAttr) {
-    override fun setupWithViewPager(viewPager: ViewPager?) {
-        super.setupWithViewPager(viewPager)
+) : FrameLayout(context, attrs, defStyleAttr), View.OnClickListener {
+
+    var onChangeCurrentItem: ((Int) -> Unit)? = null
+
+    private var showTabs = false
+
+    private val countPage: Int = context.resources.getStringArray(R.array.days_of_week).size
+
+    private lateinit var mTabLayout: CustomTabLayout
+    private lateinit var mCardLayout: MaterialCardView
+    private lateinit var mDay: MaterialTextView
+    private lateinit var mShowTabLayoutBtn: AppCompatImageButton
+    private lateinit var mCalendarBtn: AppCompatImageButton
+
+    init {
+        inflate(context, R.layout.custom_topbar, this)
+
+        initViews()
+
+        initTabLayout()
+
+        setClickListener()
+    }
+
+    fun setSelectedTab(position: Int) {
+        mTabLayout.setSelectedTab(position)
+    }
+
+    private fun initViews() {
+        mCardLayout = findViewById(R.id.top_bar_layout)
+        mDay = findViewById(R.id.top_bar_title)
+        mShowTabLayoutBtn = findViewById(R.id.top_bar_show_tabs_btn)
+        mCalendarBtn = findViewById(R.id.change_view_btn)
+        mTabLayout = findViewById(R.id.custom_tab_layout)
+    }
+
+    private fun initTabLayout() {
+        with (mTabLayout) {
+            setCountTab(countPage)
+            setSelectedPage = { selectedTab ->
+                onChangeCurrentItem?.invoke(selectedTab)
+            }
+            getSelectedTab = { selectedTab ->
+                setTextTitle(selectedTab)
+            }
+        }
+    }
+
+    private fun setTextTitle(position: Int) {
+        val arrayTextTitle = context.resources.getStringArray(R.array.full_days_of_week)
+        mDay.text = arrayTextTitle[position]
+    }
+
+    private fun setClickListener() {
+        mShowTabLayoutBtn.setOnClickListener(this)
+        mDay.setOnClickListener(this)
+        mCalendarBtn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.top_bar_show_tabs_btn, R.id.top_bar_title -> {
+                showTabs = showTabs(context, mShowTabLayoutBtn, !showTabs)
+                mTabLayout.isVisible = showTabs
+            }
+            R.id.change_view_btn -> {
+                // TODO: show calendar
+            }
+        }
     }
 }
